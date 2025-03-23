bb0288d3e096eb83a18b754072780cdf51309599
eliseevaa <eliseevaa@dialog-regions.ru>
2025-03-23 18:44:04 +0300

    fixed  element for remove list

--- homework/lexa_eliseev/lesson_4/task_1.py
+++ homework/lexa_eliseev/lesson_4/task_1.py
@@ -6,7 +6,7 @@ my_dict = {'tuple': (1, 2, 3, 4, 5),
 print(my_dict['tuple'][-1])
 
 my_dict['list'].append(999)
-my_dict['list'].pop(1)
+my_dict['list'].pop(-1)
 
 my_dict['dict']['i am a tuple'] = 'i am a tuple'
 my_dict['dict'].pop('i am a tuple')
