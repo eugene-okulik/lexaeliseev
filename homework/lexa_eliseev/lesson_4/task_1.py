77a47f8b2015d547da96dd25c0ecb7b4190aef0d
eliseevaa <eliseevaa@dialog-regions.ru>
2025-03-24 11:19:13 +0300

    fixed  element for remove list and changed key for dict

--- homework/lexa_eliseev/lesson_4/task_1.py
+++ homework/lexa_eliseev/lesson_4/task_1.py
@@ -6,10 +6,10 @@ my_dict = {'tuple': (1, 2, 3, 4, 5),
 print(my_dict['tuple'][-1])
 
 my_dict['list'].append(999)
-my_dict['list'].pop(-1)
+my_dict['list'].pop(1)
 
-my_dict['dict']['i am a tuple'] = 'i am a tuple'
-my_dict['dict'].pop('i am a tuple')
+my_dict['dict'][('i am a tuple',)] = 'i am a tuple'
+my_dict['dict'].pop(('i am a tuple',))
 
 my_dict['set'].add(000)
 my_dict['set'].remove(000)
