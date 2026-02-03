09f7ff61afda311ca86fc7c8745b8f13bd7957bd
eliseevaa <eliseevaa@dialog-regions.ru>
2026-02-03 16:10:02 +0300

    fix lint E501 line too long

--- homework/lexaeliseev/lesson_6/task_1.py
+++ homework/lexaeliseev/lesson_6/task_1.py
@@ -1,6 +1,7 @@
 """Задание 1"""
 
-text = "“Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”"
+text = "“Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at," \
+       " dignissim vitae libero”"
 
 new_text = text.split()
 
